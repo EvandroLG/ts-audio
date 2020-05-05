56d102350d09cef5ba49248fb933c98d08161c8c
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-05 23:12:31 +0200

    create getData function

--- src/audio.ts
+++ src/audio.ts
@@ -1,3 +1,11 @@
+const getData = async (file: string) => {
+  const request = new Request(file);
+  const response = await fetch(request);
+  const buffer = await response.arrayBuffer();
+
+  return buffer;
+};
+
 const Audio = (
   file: string,
   autoPlay?: boolean,
