c7db98dcf903b17b43aa73f79982c2ccaf75de4c
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-11 20:43:21 +0200

    update demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,11 +1,11 @@
-import Audio from '../../src/';
+import Audio from 'ts-audio';
 import song from './song.mp3';
 
 const getVolume = (element: HTMLInputElement) => Number(element.value) / 100;
 
 const range = <HTMLInputElement>document.getElementById('range');
 const buttonPlay = document.getElementById('bt-play');
-const audio = Audio({ file: song, loop: true, volume: 1 });
+const audio = Audio({ file: song, loop: true, volume: 0.1 });
 
 buttonPlay.addEventListener('click', () => {
   audio.play();
