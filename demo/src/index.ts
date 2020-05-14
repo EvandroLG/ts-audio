1cf045ac030721a6f5ee1474d6862839c4795ec4
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-14 20:46:54 +0200

    update example

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -5,11 +5,20 @@ const getVolume = (element: HTMLInputElement) => Number(element.value) / 100;
 
 const range = <HTMLInputElement>document.getElementById('range');
 const buttonPlay = document.getElementById('bt-play');
+const buttonPause = document.getElementById('bt-pause');
 const audio = Audio({ file: song, loop: true, volume: 0.1 });
 
 buttonPlay.addEventListener('click', () => {
   audio.play();
+
   buttonPlay.setAttribute('disabled', 'disabled');
+  buttonPause.removeAttribute('disabled');
+});
+
+buttonPause.addEventListener('click', () => {
+  audio.pause();
+  buttonPause.setAttribute('disabled', 'disabled');
+  buttonPlay.removeAttribute('disabled');
 });
 
 range.addEventListener('change', (e: Event) => {
