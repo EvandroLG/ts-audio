fb859bfa9765e6e450f8792b9c269752c93cafe4
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-03-06 23:19:50 +0100

    update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'prefer-const': 'error',
+  },
+};
