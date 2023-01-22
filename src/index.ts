2f11bc1b01edbced23f3d1a5655515f4f24d4aee
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2023-01-22 11:57:43 +0100

    removes default export to better support commonjs modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default } from './audio/Audio';
+export { default as Audio } from './audio/Audio';
 export { default as AudioPlaylist } from './playlist/AudioPlaylist';
 export * from './audio/types';
 export * from './playlist/types';
