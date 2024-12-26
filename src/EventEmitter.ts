bdf7a4f15dca4ae4327c60c54352cb5f01f50689
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-12-26 12:42:11 +0100

    Refactor: Add JSDoc comments to EventEmitter class and Event type

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,19 +1,47 @@
+/**
+ * Represents an event with associated data.
+ */
 export type Event = {
+  /**
+   * The data associated with the event. The type of data is unknown.
+   */
   data: unknown;
 };
 
+/**
+ * Event emitter class that allows registering event listeners and emitting events.
+ */
 export class EventEmitter {
+  /**
+   * A map of event keys to their respective callback functions.
+   * @private
+   */
   private events: { [key: string]: (param: Event) => void };
 
+  /**
+   * Initializes a new instance of the EventEmitter class.
+   */
   constructor() {
     this.events = {};
   }
 
-  public listener(keyEvent: string, callback: (param: Event) => void) {
+  /**
+   * Registers a listener for a specific event key.
+   *
+   * @param {string} keyEvent - The key of the event to listen for.
+   * @param {(param: Event) => void} callback - The callback function to be invoked when the event is emitted.
+   */
+  public listener(keyEvent: string, callback: (param: Event) => void): void {
     this.events[keyEvent] = callback;
   }
 
-  public emit(keyEvent: string, param: Event) {
+  /**
+   * Emits an event, invoking the corresponding listener with the provided parameter.
+   *
+   * @param {string} keyEvent - The key of the event to emit.
+   * @param {Event} param - The parameter to pass to the event's callback function.
+   */
+  public emit(keyEvent: string, param: Event): void {
     if (this.events[keyEvent]) {
       this.events[keyEvent](param);
     }
